604017af148842d841e1364183fd29b0319c9d91
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-02-14 08:41:53 +0200

    Update advanced sql commands.sql

--- advanced sql commands.sql
+++ advanced sql commands.sql
@@ -30,8 +30,55 @@
 -- select count(payment_date) from payment
 -- where extract(dow from payment_date) = 1
 
-select 
-	title, 
-	round(rental_rate/replacement_cost,2)*100 as  risk
-from film
+-- select 
+-- 	title, 
+-- 	round(rental_rate/replacement_cost,2)*100 as  risk
+-- from film
 
+--select * from customer
+--select length(email) from customer
+--select first_name ||' ' || upper(last_name) as PIB from customer
+-- select 	lower(left(first_name,2))||'.'|| lower(last_name)||'@gmail.com'
+-- as email2 from customer
+
+--select * from film
+--select avg(rental_rate) from film
+-- select * from film
+-- where rental_rate > (select avg(rental_rate) from film)
+
+-- select * from rental
+-- select * from inventory
+
+-- select film_id, title from film where film_id in
+-- (select inventory.film_id from rental
+-- inner join inventory on inventory.inventory_id = rental.inventory_id
+-- where rental_date between '2005-05-29' and '2005-05-30')
+
+-- select first_name, last_name from customer as c
+-- where exists 
+-- (select * from payment as p
+--  where 
+--  p.customer_id = c.customer_id and amount >11)
+
+
+--select * from film
+-- select avg(rental_rate) from film
+-- select title, rental_rate from film
+-- where rental_rate < (select avg(rental_rate) from film)
+
+-- select * from rental
+-- where rental_date between '2005-05-29' and '2005-05-30'
+
+-- select * from inventory
+-- inner join rental 
+-- on inventory.inventory_id = rental.inventory_id
+-- inner join film
+-- on film.film_id = inventory.film_id
+-- where rental_date between '2005-05-29' and '2005-05-30'
+
+select title, film_id from film
+inner join inventrory 
+on film.film_id = inventory.film_id
+inner join rental
+on inventory.inventory_id = rental.inventory_id
+where rental_date between '2005-05-29' and '2005-05-30'
