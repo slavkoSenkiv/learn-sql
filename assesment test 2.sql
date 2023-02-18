e1f3c97186c1c7e6457074d6cc54e5e214dc14ad
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-02-18 08:23:01 +0200

    new lessons

---
+++ assesment test 2.sql
@@ -0,0 +1,15 @@
+-- # How can you retrieve all the information from the cd.facilities table?
+-- select * from cd.facilities
+
+-- # You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?
+-- select name, membercost 
+-- from cd.facilities order by membercost asc
+
+-- # How can you produce a list of facilities that charge a fee to members?
+-- select *
+-- from cd.facilities 
+-- where membercost != 0
+-- order by membercost asc
+
+
+
