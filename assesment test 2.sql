aca5b570f3885cf0e4e4bcc3125603376c4c5d75
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-02-20 09:58:40 +0200

    Update assesment test 2.sql

--- assesment test 2.sql
+++ assesment test 2.sql
@@ -11,5 +11,21 @@
 -- where membercost != 0
 -- order by membercost asc
 
+--#How can you produce a list of facilities 
+--#that charge a fee to members, 
+--#and that fee is less than 1/50th of the monthly maintenance cost? 
+--#Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.
+
+-- select *, monthlymaintenance/50 
+-- from cd.facilities as fac1
+-- where membercost > 0 and membercost < (
+-- 	select monthlymaintenance/50 
+-- 	from cd.facilities as fac2 
+-- 	where fac1.facid = fac2.facid)
+
+
+--select * from cd.facilities where name ilike '%ten%'
+
+select * from cd.facilities where facid in (1, 5)
 
 
