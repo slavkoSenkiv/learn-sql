652fc4ba9c997f5caedf7ec0fdbd696d392e4a0a
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-02 08:40:56 +0200

    Update create databases.sql

--- create databases.sql
+++ create databases.sql
@@ -12,8 +12,28 @@
 -- 	job_name varchar(200) unique not null
 -- )
 
-create table account_job(
-	user_id integer references account(user_id),
-	job_id integer references job(job_id),
-	hire_date timestamp not null
-)
+-- create table account_job(
+-- 	user_id integer references account(user_id),
+-- 	job_id integer references job(job_id),
+-- 	hire_date timestamp not null
+-- )
+
+-- insert into account (username, password, email, create_on)
+-- values
+-- ('Slav', 'qwe123', 'slav@gmail.com', current_timestamp)
+
+-- insert into account (user_id, username, password, email, create_on, last_login)
+-- values
+-- (2, 'Slav2', 'qwe123', 'slav2@gmail.com', current_timestamp, current_timestamp)
+
+-- insert into account (username, password, email, create_on)
+-- values
+-- ('Paul2', 'qwe123', 'slav7@gmail.com', current_timestamp),
+-- ('Nick2', 'qwe123', 'slav8@gmail.com', current_timestamp)
+
+--insert into job (job_name) values ('Tailor'), ('Janitor')
+
+--insert into account_job (user_id, job_id, hire_date) values (1, 1, current_timestamp)
+--insert into account_job (user_id, job_id, hire_date) values (1, 2, current_timestamp)
+--insert into account_job (user_id, job_id, hire_date) values (2, 2, current_timestamp)
+insert into account_job (user_id, job_id, hire_date) values (2, 2, current_timestamp)
