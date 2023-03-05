781637015c19dfd99e7c8974840eeecf7ecfb9e8
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-05 08:54:16 +0200

    Create assesment test 3.sql

---
+++ assesment test 3.sql
@@ -0,0 +1,28 @@
+-- create database school
+
+-- create table students(
+-- 	syudent_id serial primary key,
+-- 	first_name varchar(50) not null,
+-- 	last_name varchar(50) not null,
+-- 	homeroom_number integer not null unique,
+-- 	phone varchar(50),
+-- 	email varchar(50) unique,
+-- 	graduation_year date
+-- )
+
+-- ALTER TABLE students
+-- RENAME COLUMN syudent_id TO students_id;
+
+-- create table teachers(
+-- 	teacher_id serial primary key,
+-- 	first_name varchar(50) not null,
+-- 	last_name varchar(50) not null,
+-- 	homeroom_number integer not null unique,
+-- 	phone varchar(50),
+-- 	email varchar(50) unique,
+-- 	department varchar(50))
+
+-- # not working - modify datatype and constraint
+-- ALTER TABLE students 
+-- modify phone varchar(50) not null;
+
