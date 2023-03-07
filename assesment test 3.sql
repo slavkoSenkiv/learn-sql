4ec541aa59bee3e28ea66686d3d586d9912c10b7
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-03-07 08:27:23 +0200

    Update assesment test 3.sql

--- assesment test 3.sql
+++ assesment test 3.sql
@@ -42,8 +42,40 @@
 
 --ALTER TABLE teachers alter department set not null;
 
--- # want but cant add value to existing row - change last department column value
-insert into teachers (department) values ('art')
+-- update teachers set department = 'art' where teacher_id = 9
+
+-- select * from students
+-- SELECT *
+-- FROM information_schema.columns
+-- WHERE table_name = 'students';
+
+-- SELECT columns.column_name, constraints.constraint_type, constraints.constraint_name
+-- FROM information_schema.columns AS columns
+-- LEFT JOIN information_schema.constraint_column_usage AS usage
+-- ON columns.table_catalog = usage.table_catalog
+-- AND columns.table_schema = usage.table_schema
+-- AND columns.table_name = usage.table_name
+-- AND columns.column_name = usage.column_name
+-- LEFT JOIN information_schema.table_constraints AS constraints
+-- ON usage.constraint_catalog = constraints.constraint_catalog
+-- AND usage.constraint_schema = constraints.constraint_schema
+-- AND usage.constraint_name = constraints.constraint_name
+-- WHERE columns.table_name = 'students';
+
+-- insert into students 
+-- (first_name, last_name, homeroom_number, phone, email)
+-- values
+-- ('slav', 'senkiv', 2, '123', 'asd')
+
+ALTER TABLE students
+ADD CONSTRAINT unique_data UNIQUE (phone);
+
+-- update students set phone = '234' where students_id = 4
+
+
+ 
+
+
 
 
 
